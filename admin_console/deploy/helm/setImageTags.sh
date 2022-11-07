0d244e9534c1e5c8dc25323b94f916eb513a3f3f
root <6ZEHbNOwH3I02Sf9pDH6ETXWxTnSNCiGAEiPHliFYVHwdZDWeno9lC49xradfNXP>
2022-11-07 12:39:21 +0300

    update

--- admin_console/deploy/helm/setImageTags.sh
+++ admin_console/deploy/helm/setImageTags.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-sed -i "s/\${tags}/$1/g" ./helm/values.yaml
+sed -i "s/\${tags}/$1/g" /kaniko/workspace/datagram_ci_cd/admin_console/deploy/helm/values.yaml
--- admin_console/deploy/helm/setRevision.sh
+++ admin_console/deploy/helm/setRevision.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-sed -i "s/\${revision}/$1/g" ./helm/Chart.yaml
+sed -i "s/\${revision}/$1/g" /kaniko/workspace/datagram_ci_cd/admin_console/deploy/helm/Chart.yaml
