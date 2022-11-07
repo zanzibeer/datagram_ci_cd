7328154a2311b0dd969754b426544bedb34c614b
root <6ZEHbNOwH3I02Sf9pDH6ETXWxTnSNCiGAEiPHliFYVHwdZDWeno9lC49xradfNXP>
2022-11-07 16:18:19 +0300

    update

--- admin_console/build/setUrl.sh
+++ admin_console/build/setUrl.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-sed -i "s/\localhost/admin-console-postgresql/g" /kaniko/workspace/datagram-admin-console/dg-admin-console-bk/src/main/resources/application.yml
+sed -i "s/localhost/admin-console-postgresql/g" /kaniko/workspace/datagram-admin-console/dg-admin-console-bk/src/main/resources/application.yml
