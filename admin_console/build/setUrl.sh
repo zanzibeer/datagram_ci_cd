13242af30862bd54e8da91dcb5ea4fa770921773
root <6ZEHbNOwH3I02Sf9pDH6ETXWxTnSNCiGAEiPHliFYVHwdZDWeno9lC49xradfNXP>
2022-11-08 11:47:05 +0300

    update

--- admin_console/build/setUrl.sh
+++ admin_console/build/setUrl.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-sed -i "s/postgres/$1/g" /kaniko/workspace/datagram-admin-console/dg-admin-console-bk/src/main/resources/application.yml
+sed -i "s/postgres/$1/g" /kaniko/workspace/datagram-admin-console/dg-admin-console-bk/src/main/resources/application-docker.yml
